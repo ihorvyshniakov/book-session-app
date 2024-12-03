263625c013d4204bb3fa75a728e7c54cc9084734
Vyshniakov Ihor <gramazekavip@gmail.com>
2024-12-03 05:46:45 +0100

    styling fix of Input

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,16 +1,15 @@
 import { type ComponentPropsWithoutRef } from 'react'
 
 type InputProp = {
+	id: string
 	label: string
 } & ComponentPropsWithoutRef<'input'>
 
-const Input = ({ label, ...otherProps }: InputProp) => {
+const Input = ({ id, label, ...otherProps }: InputProp) => {
 	return (
 		<div className='control'>
-			<label>
-				{label}
-				<input {...otherProps} />
-			</label>
+			<label htmlFor={id}>{label}</label>
+			<input id={id} {...otherProps} />
 		</div>
 	)
 }
