57159c6dcbe36c2ad97f9df2905e6cc4871e254b
Vyshniakov Ihor <gramazekavip@gmail.com>
2024-12-03 08:00:02 +0100

    fixed error with routing, added base URL

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import SessionsPage from './pages/Sessions.tsx'
 import SessionPage from './pages/Session.tsx'
 import Root from './pages/Root.tsx'
 
-const Router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-		children: [
-			{
-				index: true,
-				element: <HomePage />
-			},
-			{ path: 'sessions', element: <SessionsPage /> },
-			{ path: 'sessions/:id', element: <SessionPage /> }
-		]
-	}
-])
+const Router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Root />,
+			children: [
+				{
+					index: true,
+					element: <HomePage />
+				},
+				{ path: 'sessions', element: <SessionsPage /> },
+				{ path: 'sessions/:id', element: <SessionPage /> }
+			]
+		}
+	],
+	{ basename: '/book-session-app' }
+)
 
 function App() {
 	return <RouterProvider router={Router} />
